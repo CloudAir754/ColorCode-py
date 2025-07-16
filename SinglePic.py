aafe9ec91c3bb3baf1cd1d55bd992f7a8cf2741d
CloudAir754 <cloudair754@gmail.com>
2025-07-17 00:25:33 +0800

    v1.0.3，小修单照片输出

--- SinglePic.py
+++ SinglePic.py
@@ -1,7 +1,7 @@
 from ColorCodeDetector.ColorCodeDetector import ColorCodeDetector
 
 PicPath = "./Sample/temp/Ori_"
-file = "88"
+file = "46"
 
 PicPath = PicPath + file + ".jpg"
 
@@ -9,8 +9,25 @@ picSolve = ColorCodeDetector(PicPath,True)
 
 if __name__ == "__main__":
     picSolve.show_steps = True  # 控制是否显示处理步骤
-    picSolve.show_details = True # 展示hsv
-    picSolve.HPbrightness_threshold = 60  # 亮度通道阈值，高于此值则加亮度 120 ）
-    picSolve.HP_gamma = 0.4  # 指数映射比率 0.7）
-    result = picSolve.analyze()
-    print("AAA")
+    picSolve.show_details = False # 展示hsv
+    picSolve.HPbrightness_threshold = 120  # 亮度通道阈值，高于此值则加亮度 120 ）
+    picSolve.HP_gamma = 0.7  # 指数映射比率 0.7）
+    result = picSolve.analyze()    
+
+    print("\n\n\n\n")
+    print("="*50)
+    print("Recognition result")
+    # 格式化拉伸比
+    stretch_ratio = round(float(1/result["stretch_ratio"]), 3)
+    # stretch_ratio = 2.01 # 用于调整输出
+    print(f"Streching radio:     {stretch_ratio}")
+       
+    # 格式化颜色矩阵为3行
+    color_matrix = "\n".join(
+        [f"    {str(row):<30}" for row in result['color_matrix']]
+    )
+    print("Color matrix:")
+    print(color_matrix)
+    print("="*50)
+    print("\n\n\n\n")
+    
